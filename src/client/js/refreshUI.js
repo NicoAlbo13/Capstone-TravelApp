1ad3bdea540e9faf180dc7d349f70407560f751c
Nicolás Albornoz <nico.albornoz.s@gmail.com>
2022-02-11 19:22:24 -0500

    feat: Made the city first letter always upper case

--- src/client/js/refreshUI.js
+++ src/client/js/refreshUI.js
@@ -1,4 +1,5 @@
 const refreshUI = async (city) => {
+    const Ccity =  city.charAt(0).toUpperCase() + city.slice(1);
     const request = await fetch('http://localhost:8081/all');
     try{
         const allData = await request.json();
@@ -8,9 +9,9 @@ const refreshUI = async (city) => {
         document.getElementById('max').innerHTML = `Maximun: ${allData[0].maxTemp}`;
         document.getElementById('description').innerHTML = `${allData[0].description}`;
         document.getElementById('days-away').innerHTML = `You are ${allData[0].days} days away from your trip.`;
-        document.getElementById('general').innerHTML = `The city of ${city} you are visiting is located in ${allData[1].region}.
-                                                        ${city} is located in the ${allData[1].name} and its capital is ${allData[1].capital}.
-                                                        The total population of the country is ${allData[1].population}. The language they 
+        document.getElementById('general').innerHTML = `The city of ${Ccity} you are visiting is located in ${allData[1].region}.
+                                                        ${Ccity} is located in the ${allData[1].name} and its capital is ${allData[1].capital}.
+                                                        The total population of the country is ${allData[1].population}. The language they
                                                         speak is ${allData[1].lan}. The money they use is the ${allData[1].money}.`;
     }catch(error){
         console.log(error);
