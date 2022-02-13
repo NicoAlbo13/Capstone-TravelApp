bc631a467e85f0ccfb4bc3059ce03bea113e633c
Nicolás Albornoz <nico.albornoz.s@gmail.com>
2022-02-13 16:49:19 -0500

    feat: Add styles with sass

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,4 +1,4 @@
-import { refreshUI } from "..";
+import { refreshUI } from "./refreshUI";
 
 document.getElementById('submit').addEventListener('click', handleSubmit);
 
@@ -7,20 +7,34 @@ async function handleSubmit(e){
     //Get geo location of the destination provided by de user
     let place = document.getElementById('destination').value;
     let date = document.getElementById('departDate').value;
+    let error = document.getElementById('error');
+    let results = document.getElementById('result-container');
     let geoInfo = await Client.getGeo(place, date);
     let weather = await Client.getWeather(geoInfo);
     let image = await Client.getImage(geoInfo);
     let general = await Client.getGeneral(geoInfo);
     if (place === '' || date === ''){
+        error.classList.remove('hide');
+        results.classList.add('hide');
         if (place === '' & date === ''){
-            document.getElementById('error').innerHTML = "Insert place and date to continue";
+            document.getElementById('error').innerHTML = "&#9888; Enter place and date to continue";
         }else if (date === ''){
-            document.getElementById('error').innerHTML = "Insert a date to continue";
+            document.getElementById('error').innerHTML = "&#9888; Enter a date to continue";
         }else{
-            document.getElementById('error').innerHTML = "Insert a place to continue";
+            document.getElementById('error').innerHTML = "&#9888; Enter a place to continue";
         }
     }else{
-        setTimeout(refreshUI, 100, place)
+        const travelDate = new Date(date);
+        const actualDate = new Date();
+        if(travelDate<actualDate){
+            document.getElementById('error').innerHTML = "&#9888; Enter a valid date ";
+        }else{
+            error.classList.add('hide');
+            results.classList.remove('hide');
+            setTimeout(refreshUI, 100, place);
+            document.getElementById('destination').value = '';
+            document.getElementById('departDate').value = '';
+        }
     }
 }
 
--- src/client/js/refreshUI.js
+++ src/client/js/refreshUI.js
@@ -4,17 +4,18 @@ const refreshUI = async (city) => {
     try{
         const allData = await request.json();
         const iconUrl = `https://www.weatherbit.io/static/img/icons/${allData[0].icon}.png`;
+        const population = allData[1].population.toLocaleString("en-US");
         document.getElementById('img').setAttribute("src", allData[2].img);
-        document.getElementById('temp').innerHTML = `Temperature: ${allData[0].temp}`;
-        document.getElementById('min').innerHTML = `Lowest: ${allData[0].minTemp}`;
-        document.getElementById('max').innerHTML = `Maximun: ${allData[0].maxTemp}`;
+        document.getElementById('Trip').innerHTML = `Trip to: ${Ccity}`;
+        document.getElementById('temp').innerHTML = `<b class='bold'>Temperature:</b> ${allData[0].temp}&deg;C`;
+        document.getElementById('range').innerHTML = `<b class='bold'>MIN:</b> ${allData[0].minTemp}&deg;C <b class='bold'>MAX:</b> ${allData[0].maxTemp}&deg;C`;
         document.getElementById('iconImg').setAttribute("src", iconUrl);
-        document.getElementById('description').innerHTML = `${allData[0].description}`;
-        document.getElementById('days-away').innerHTML = `You are ${allData[0].days} days away from your trip.`;
-        document.getElementById('general').innerHTML = `The city of ${Ccity} you are visiting is located in ${allData[1].region}.
-                                                        ${Ccity} is located in the ${allData[1].name} and its capital is ${allData[1].capital}.
-                                                        The total population of the country is ${allData[1].population}. The language they
-                                                        speak is ${allData[1].lan}. The money they use is the ${allData[1].money}.`;
+        document.getElementById('description').innerHTML = `We forcast <b class='bold'>${allData[0].description}</b>`;
+        document.getElementById('days-away').innerHTML = `You are <b class='bold'><i>${allData[0].days} days away</i></b> from your trip.`;
+        document.getElementById('general').innerHTML = `The city of ${Ccity} you are visiting is located in <b class='bold'>${allData[1].region}</b>.
+                                                        ${Ccity} is located in the <b class='bold'>${allData[1].name}</b> and its capital is <b class='bold'>${allData[1].capital}</b>.
+                                                        The total population of the country is <b class='bold'>${population}</b>. They
+                                                        speak <b class='bold'>${allData[1].lan}</b> and the money they use is the <b class='bold'>${allData[1].money}</b>.`;
     }catch(error){
         console.log(error);
     }
